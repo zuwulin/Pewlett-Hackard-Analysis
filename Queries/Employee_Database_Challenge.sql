11126daa40adcd7eb65537aca98ab78eeaecd196
unknown <lesprimora@gmail.com>
2022-04-10 23:33:45 -0400

    mentorship query added

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,6 +1,6 @@
 
 --Create retirement by titles table (for 1952 - 1955)
-select e.emp_no,
+SELECT e.emp_no,
 e.first_name,
 e.last_name,
 t.title,
@@ -29,3 +29,21 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT DESC;
+
+--Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT OUTER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN title as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+    AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
